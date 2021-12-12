7eaeddac75158d0644b3f87c7f5f1d52c394594a
Walid Wazini <wazini25@gmail.com>
2021-12-12 21:41:16 +0800

    native event object

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: '',
+      name1: '',
       name2: '',
     };
   },
   methods:{
-   
+    setName1(name){
+      this.name1 =  event.target.value
+    },
+
+    setName2(event, lastName){
+      this.name2 = event.target.value + " " + lastName
+    },
+
     increment(num){
       this.counter = this.counter + num
     }, 
